81cc151dcc8ea703ed5a5967078b367edd3c69f5
HeHao <1036566848@qq.com>
2022-09-24 16:27:35 +0800

    notebook

---
+++ src/main/java/practice/SC.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class SC {
+    private String sId;
+    private String cId;
+    private Float score;
+
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
+
+    public String getcId() {
+        return cId;
+    }
+
+    public void setcId(String cId) {
+        this.cId = cId;
+    }
+
+    public Float getScore() {
+        return score;
+    }
+
+    public void setScore(Float score) {
+        this.score = score;
+    }
+}
